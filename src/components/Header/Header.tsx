e462d2b27781e9d90f73f4148eb8044865e9ccbb
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-04-03 01:57:21 +0700

    Enhance Header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,52 @@
-import { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Selling', href: '#', current: false },
-  { name: 'Stock', href: '#', current: false },
-  { name: 'Purchasing', href: '#', current: false }
+const mainNavs = [
+  { id: 'dashboard', name: 'dashboard', label: 'Dashboard', href: '#dashboard' },
+  { id: 'stock', name: 'stock', label: 'Stock', href: '#stock' },
+  { id: 'selling', name: 'selling', label: 'Selling', href: '#selling' },
+  { id: 'purchasing', name: 'purchasing', label: 'Purchasing', href: '#purchasing' }
 ]
 
-const getProfileClass = (isActive: boolean): string => {
-  const mainClass = 'block px-4 py-2 text-sm text-gray-700'
+const accountNavs = [
+  { id: 'profile', name: 'profile', label: 'Profile', href: '#profile' },
+  { id: 'settings', name: 'settings', label: 'Settings', href: '#settings' },
+  { id: 'signout', name: 'signout', label: 'Sign Out', href: '#signout' }
+]
+
+const handleAccountNav = (isActive: boolean): string => {
+  const mainClass = 'block px-4 py-2 text-sm text-gray-700 w-full text-left'
   return isActive ? `bg-gray-100 ${mainClass}` : mainClass
 }
 
-const getNavClass = (isActive: boolean): string => {
-  const mainClass = 'px-3 py-2 rounded-md text-sm font-medium'
-  return isActive ? `bg-gray-900 text-white ${mainClass}` : `text-gray-300 hover:bg-gray-700 hover:text-white ${mainClass}`
-}
-
-const getDisclosurelass = (isActive: boolean): string => {
-  const mainClass = 'block px-3 py-2 rounded-md text-base font-medium'
-  return isActive ? `bg-gray-900 text-white ${mainClass}` : `text-gray-300 hover:bg-gray-700 hover:text-white ${mainClass}`
-}
-
 const Header = (): JSX.Element => {
+  /**
+   * States
+   */
+
+  const [activeNav, setActiveNav] = useState('dashboard')
+
+  /**
+   * Effects
+   */
+
+  useEffect(() => {
+    console.log('activeNav', activeNav)
+  }, [activeNav])
+
+  /**
+   * Handlers
+   */
+
+  const handleNav = (nav: string): void => {
+    setActiveNav(nav)
+  }
+
+  /**
+   * Renders
+   */
+
   return (
     <Disclosure as='nav' className='bg-gray-800'>
       {({ open }) => (
@@ -41,7 +63,7 @@ const Header = (): JSX.Element => {
                     : <MenuIcon className='block h-6 w-6' aria-hidden='true' />}
                 </Disclosure.Button>
               </div>
-              {/* */}
+              {/* Desktop Navigation */}
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex-shrink-0 flex items-center'>
                   <img className='block lg:hidden h-8 w-auto' src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg' alt='Workflow' />
@@ -49,27 +71,30 @@ const Header = (): JSX.Element => {
                 </div>
                 <div className='hidden sm:block sm:ml-6'>
                   <div className='flex space-x-4'>
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={getNavClass(item.current)}
-                        aria-current={item.current ? 'page' : undefined}
+                    {mainNavs.map((nav) => (
+                      <button
+                        key={nav.id}
+                        // href={nav.href}
+                        className={activeNav === nav.id
+                          ? 'bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'
+                          : 'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'}
+                        aria-current={activeNav === nav.id ? 'page' : undefined}
+                        onClick={() => handleNav(nav.id)}
                       >
-                        {item.name}
-                      </a>
+                        {nav.label}
+                      </button>
                     ))}
                   </div>
                 </div>
               </div>
-              {/* */}
+              {/* Right Navigation */}
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                {/* Notification Bell */}
+                {/* Bell Button */}
                 <button type='button' className='bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
                   <span className='sr-only'>View notifications</span>
                   <BellIcon className='h-6 w-6' aria-hidden='true' />
                 </button>
-                {/* Profile Dropdown */}
+                {/* Account Menu */}
                 <Menu as='div' className='ml-3 relative'>
                   <div>
                     <Menu.Button className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
@@ -87,27 +112,15 @@ const Header = (): JSX.Element => {
                     leaveTo='transform opacity-0 scale-95'
                   >
                     <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a href='/' className={getProfileClass(active)}>
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a href='/' className={getProfileClass(active)}>
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a href='/' className={getProfileClass(active)}>
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
+                      {accountNavs.map((nav) => (
+                        <Menu.Item key={nav.id}>
+                          {({ active }) => (
+                            <button className={handleAccountNav(active)}>
+                              {nav.label}
+                            </button>
+                          )}
+                        </Menu.Item>
+                      ))}
                     </Menu.Items>
                   </Transition>
                 </Menu>
@@ -116,19 +129,22 @@ const Header = (): JSX.Element => {
               {/* */}
             </div>
           </div>
-          {/* */}
+          {/* Mobile Navigation */}
           <Disclosure.Panel className='sm:hidden'>
             <div className='px-2 pt-2 pb-3 space-y-1'>
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as='a'
-                  href={item.href}
-                  className={getDisclosurelass(item.current)}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
+              {mainNavs.map((nav) => (
+                <div key={nav.id} onClick={() => handleNav(nav.id)}>
+                  <Disclosure.Button
+                    // as='a'
+                    // href={nav.href}
+                    className={activeNav === nav.id
+                      ? 'bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'}
+                    aria-current={activeNav === nav.id ? 'page' : undefined}
+                  >
+                    {nav.label}
+                  </Disclosure.Button>
+                </div>
               ))}
             </div>
           </Disclosure.Panel>
