86178d85a54afdff37136a4472ffe0ffaacb34a5
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-04-03 03:24:18 +0700

    Implement Contents in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import {
   Main,
@@ -6,12 +7,33 @@ import {
   Content
 } from './components'
 
+import {
+  Dashboard,
+  Stock,
+  Selling,
+  Purchasing,
+  Profile,
+  Settings,
+  Signout
+} from './contents'
+
 const App = (): JSX.Element => {
   return (
     <Main>
       <>
         <Header />
-        <Content />
+        <Content>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/stock' element={<Stock />} />
+            <Route path='/selling' element={<Selling />} />
+            <Route path='/purchasing' element={<Purchasing />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/signout' element={<Signout />} />
+          </Routes>
+        </Content>
       </>
     </Main>
   )
