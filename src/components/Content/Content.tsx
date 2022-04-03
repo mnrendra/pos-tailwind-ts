411bbdc6fc5782456b277e935cf07b36b34bf899
Muhammad Rendra <muhammadnurrendra@gmail.com>
2022-04-03 18:35:28 +0700

    fixing width and height on Content component.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const Content = ({ children }: Props): JSX.Element => {
   return (
-    <main>
-      <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+    <main className='flex w-full h-full bg-gray-100'>
+      <div className='w-full max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
         {children === undefined || children === null
           ? (
             <div className='px-4 py-6 sm:px-0'>
